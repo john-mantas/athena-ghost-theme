e7a58e12b0ac95b41230628aa02c24eaca9d88a7
John Mantas <mantasjohn67@gmail.com>
2024-02-18 15:51:24 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// JavaScript files are compiled and minified during the build process to the
+// assets folder. See available scripts in the package.json file.
+
+// Import CSS
+import "../css/index.css";
+
+console.log("Athena Ghost theme");
